f7959c0d631e52f21e26b27adf17e70fb4fa184d
Nicholas E. Voelker <volkoff223@gmail.com>
2021-02-10 21:02:11 -0500

    First commit

--- models/review.js
+++ models/review.js
@@ -3,11 +3,7 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     body: String,
-    rating: Number,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
+    rating: Number
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
